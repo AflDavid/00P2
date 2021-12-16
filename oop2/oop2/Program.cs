a1de6088f26dfaf264c2932bab8694be56da13ef
AflDavid <93366100+AflDavid@users.noreply.github.com>
2021-12-17 01:32:10 +0200

    Add files via upload

---
+++ oop2/oop2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facultate UEO = new Facultate("UEO");
+            UEO.Students = new Student[] {new Student("Afloarei", "David", new DateTime(2001, 08, 21), "112233"),
+                              new Student("Andrei", "Daniel", new DateTime(2002, 01, 02), "543210"),
+                              new Student("Ulici", "Raul", new DateTime(2002, 01, 03), "012345")};
+
+
+            Console.WriteLine(UEO.Index("112233"));
+
+
+        }
+    }
+}
+
